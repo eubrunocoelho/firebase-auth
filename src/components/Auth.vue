ef4467ded4e1e0d01d863112d65fe4618feb5b28
Bruno Coelho <eu.brunocoelho94@gmail.com>
2023-10-17 13:56:55 -0300

    created registration form

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -1,11 +1,13 @@
 <template>
     <header class="tab-header">
         <nav class="tab-navigation">
-            <a href="#" class="active">Login</a>
-            <a href="#">Cadastro</a>
+            <!-- <a href="#" class="active">Login</a> -->
+            <a href="#">Login</a>
+            <a href="#" class="active --red">Cadastro</a>
         </nav>
     </header>
-    <section class="container">
+    <!-- Login -->
+    <section class="container" v-if="false">
         <div class="title">
             <h1>Logar-se</h1>
         </div>
@@ -33,6 +35,38 @@
             <p>Mensagem de erro.</p>
         </div>
     </section>
+    <!-- End Login -->
+    <!-- Cadastro -->
+    <section class="container">
+        <div class="title">
+            <h1 class="--red">Cadastre-se</h1>
+        </div>
+        <form action="#" class="form">
+            <div class="form-control">
+                <label for="name">Nome</label>
+                <input type="text" id="name" name="name" class="--red" placeholder="Entre com seu nome...">
+            </div>
+            <div class="form-control">
+                <label for="email">Endereço de E-mail</label>
+                <input type="email" id="email" name="email" class="--red" placeholder="Entre com seu endereço de e-mail...">
+            </div>
+            <div class="form-control">
+                <label for="password">Senha</label>
+                <input type="password" id="password" name="password" class="--red" placeholder="Entre com sua senha...">
+            </div>
+            <button type="submit" class="btn --red">Cadastrar</button>
+        </form>
+        <div class="alert">
+            <p>Mensagem padrão.</p>
+        </div>
+        <div class="alert alert--success">
+            <p>Mensagem de sucesso.</p>
+        </div>
+        <div class="alert alert--danger">
+            <p>Mensagem de erro.</p>
+        </div>
+    </section>
+    <!-- End Cadastro -->
 </template>
 
 <script>

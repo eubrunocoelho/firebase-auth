5002ba6cc734c5d152f8fa6ef45b208a38a297b8
Bruno Coelho <eu.brunocoelho94@gmail.com>
2023-10-19 14:19:53 -0300

    changes in style

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,18 +1,42 @@
 <template>
-    <section class="container">
+    <section class="container" v-if="true">
         <div class="dashboard-heading">
             <h1><span>#</span>Dashboard</h1>
         </div>
         <div class="summary">
             <p>Olá, Bruno Coelho!</p>
+            <button class="btn">Sair</button>
         </div>
-        <button class="btn">Sair</button>
     </section>
 </template>
 
 <script>
+import { useStore } from 'vuex';
+import { useRouter } from 'vue-router';
+import { computed } from 'vue';
+import { auth } from '../firebaseConfig';
+
 export default {
-    name: 'DashboardComponent'
+    name: 'DashboardComponent',
+    setup() {
+        const store = useStore();
+        const router = useRouter();
+
+        auth.onAuthStateChanged(user => {
+            store.dispatch('fetchUser', user);
+        });
+
+        const user = computed(() => {
+            return store.getters.user;
+        });
+
+        const signOut = async () => {
+            await store.dispatch('logOut')
+            router.push('/');
+        };
+
+        return { user, signOut };
+    }
 }
 </script>
 

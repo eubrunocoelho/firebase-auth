3bb5f45d3f0f66b58ab34061c53f213a16078599
Bruno Coelho <eu.brunocoelho94@gmail.com>
2023-10-20 13:42:32 -0300

    created redirection system

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,29 +1,61 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import { auth } from '../firebaseConfig';
 import AppComponent from '../App';
 import AuthComponent from '../components/Auth';
 import DashbaordComponent from '../components/Dashboard';
+
+let user = null;
+
+auth.onAuthStateChanged((authUser) => {
+    user = authUser;
+});
 
 const routes = [
     {
         path: '/',
         name: 'App',
-        component: AppComponent
+        component: AppComponent,
+        meta: {
+            requiresAuth: false
+        }
     },
     {
         path: '/dashboard',
         name: 'Dashboard',
-        component: DashbaordComponent
+        component: DashbaordComponent,
+        meta: {
+            requiresAuth: true
+        }
     },
     {
         path: '/auth',
         name: 'AuthComponent',
-        component: AuthComponent
+        component: AuthComponent,
+        meta: {
+            requiresAuth: false
+        }
     }
-]
+];
 
 const router = createRouter({
     history: createWebHistory(),
     routes
+});
+
+router.beforeEach((to, from, next) => {
+    if (!user && to.meta.requiresAuth) {
+        next('Dashboard');
+    } else {
+        next();
+    }
+});
+
+router.beforeEach((to, from, next) => { 
+    if (user && !to.meta.requiresAuth) {
+        next('Auth');
+    } else {
+        next();
+    }
 });
 
 export default router;
